9b9ad96c427afcb18291aa1126a30c1a96223952
Aaron-Wright <georonathan47@gmail.com>
2020-11-12 20:28:50 +0000

    adjustments

--- lib/features/presentation/pages/profile.dart
+++ lib/features/presentation/pages/profile.dart
@@ -1,8 +1,7 @@
 import 'dart:ui';
 
-import 'package:flutter/material.dart';
-
 import 'sidebar.dart';
+import 'package:flutter/material.dart';
 
 void main() => runApp(new Profile());
 
@@ -16,44 +15,98 @@ class ProfileState extends State<Profile> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: AppBar(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         title: Text('Profile'),
+        elevation: 2,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.cyan,
+          ),
+          onPressed: () {
+            Sidebar();
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.settings,
+              color: Colors.cyan,
+            ),
+            onPressed: () {},
+          ),
+        ],
       ),
-      drawer: Sidebar(),
-      body: new Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          new Form(
-            child: new Container(
-              padding: const EdgeInsets.all(30),
-              child: new Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  new Text(
-                    'EDIT YOUR PROFILE',
-                    style: TextStyle(
-                      fontSize: 22,
-                      color: Colors.cyan,
-                    ),
-                  ),
-                  new Container(
-                    child: ListTile(
-                      leading: Icon(Icons.edit),
-                      title: Text(
-                        'Edit Profile',
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.cyanAccent,
-                        ),
+      //drawer: Sidebar(),
+      body: new Container(
+        padding: EdgeInsets.only(
+          left: 16,
+          right: 16,
+          top: 25,
+        ),
+        child: ListView(
+          children: [
+            Text(
+              'Edit Profile',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Center(
+              child: new Stack(
+                children: [
+                  Container(
+                    width: 140,
+                    height: 140,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 4,
+                        color: Theme.of(context).scaffoldBackgroundColor,
                       ),
-                      onTap: () {},
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 12,
+                          spreadRadius: 3.5,
+                          color: Colors.cyanAccent.withOpacity(0.5),
+                          offset: Offset(0, 15),
+                        ),
+                      ],
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(""),
+                      ),
                     ),
                   ),
-                  
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          width: 4,
+                          color: Theme.of(context).scaffoldBackgroundColor,
+                        ),
+                        color: Colors.cyan,
+                      ),
+                      child: Icon(
+                        Icons.edit,
+                        color: Colors.cyanAccent,
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
